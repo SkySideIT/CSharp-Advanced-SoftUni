fb9ffec1ab938da977b1ea5692e4f6b41b0c5f62
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-09-07 19:36:25 +0300

    Add files via upload

---
+++ Exercises/StackAndQueues/03.MaximumAndMinimumElement/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _03.MaximumAndMinimumElement;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Stack<int> stack = new Stack<int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int command = input[0];
+
+            if (command == 1)
+            {
+                stack.Push(input[1]);
+            }
+            else if (command == 2)
+            {
+                stack.Pop();
+            }
+            else if (command == 3)
+            {
+                Console.WriteLine(stack.Max());
+            }
+            else
+            {
+                Console.WriteLine(stack.Min());
+            }
+        }
+
+        Console.WriteLine(string.Join(", ", stack));
+    }
+}
