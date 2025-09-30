60be79887795c4181b26139d690670924bdb1e70
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-10-01 02:55:45 +0300

    Add files via upload

---
+++ Exercises/StreamsFilesAndDirectories/CopyDirectory/CopyDirectory.cs
@@ -0,0 +1,33 @@
+﻿namespace CopyDirectory;
+
+using System;
+using System.IO;
+
+public class CopyDirectory
+{
+    static void Main()
+    {
+        string inputPath =  Console.ReadLine();
+        string outputPath = Console.ReadLine();
+
+        CopyAllFiles(inputPath, outputPath);
+    }
+
+    public static void CopyAllFiles(string inputPath, string outputPath)
+    {
+        if (Directory.Exists(outputPath))
+        {
+            Directory.Delete(outputPath, recursive: true);
+        }
+
+        Directory.CreateDirectory(outputPath);
+
+        foreach (string pathToFile in Directory.GetFiles(inputPath))
+        {
+            string fileName = Path.GetFileName(pathToFile);
+            string pathToDestination = Path.Combine(outputPath, fileName);
+
+            File.Copy(pathToFile, pathToDestination);
+        }
+    }
+}
