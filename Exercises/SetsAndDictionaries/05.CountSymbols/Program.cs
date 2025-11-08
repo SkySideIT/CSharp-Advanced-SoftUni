9540158be1d343bcb4426de7f5cafa42e19c8c4f
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-11-08 21:56:16 +0200

    Add files via upload

---
+++ Exercises/SetsAndDictionaries/05.CountSymbols/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _05.CountSymbols;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string text = Console.ReadLine();
+
+        Dictionary<char, int> charMap = new Dictionary<char, int>();
+
+        foreach (char ch in text)
+        {
+            if (!charMap.ContainsKey(ch))
+            {
+                charMap[ch] = 0;
+            }
+
+            charMap[ch]++;
+        }
+
+        foreach (char ch in charMap.Keys.OrderBy(x => x))
+        {
+            Console.WriteLine($"{ch}: {charMap[ch]} time/s");
+        }
+    }
+}
