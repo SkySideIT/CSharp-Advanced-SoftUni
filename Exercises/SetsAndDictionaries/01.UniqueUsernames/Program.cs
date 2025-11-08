9df40934a81b6b169f95d79a3b90c9b0d837e26e
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-11-08 21:53:58 +0200

    Add files via upload

---
+++ Exercises/SetsAndDictionaries/01.UniqueUsernames/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _01.UniqueUsernames;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        HashSet<string> usernames = new HashSet<string>();
+
+        for (int i = 0; i < n; i++)
+        {
+            usernames.Add(Console.ReadLine());
+        }
+
+        foreach (string name in usernames)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
