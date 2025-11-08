56a213c1ad5d5e6a2e05a13fdf4ca0f1a013759b
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-11-08 21:55:00 +0200

    Add files via upload

---
+++ Exercises/SetsAndDictionaries/03.PeriodicTable/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+4
+Ce O
+Mo O Ce
+Ee
+Mo
+ 
+*/
+
+namespace _03.PeriodicTable;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        HashSet<string> set = new HashSet<string>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] input = Console.ReadLine().Split();
+            foreach (string el in input)
+            {
+                set.Add(el);
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", set.OrderBy(x => x)));
+    }
+}
