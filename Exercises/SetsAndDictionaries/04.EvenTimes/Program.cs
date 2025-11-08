a683524811325e8d0687c01082579f763b4c14bf
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-11-08 21:55:36 +0200

    Add files via upload

---
+++ Exercises/SetsAndDictionaries/04.EvenTimes/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _04.EvenTimes;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Dictionary<int, int> countTimes = new Dictionary<int, int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+            if (!countTimes.ContainsKey(number))
+            {
+                countTimes[number] = 0;
+            }
+
+            countTimes[number]++;
+        }
+
+        foreach (int number in countTimes.Keys)
+        {
+            if (countTimes[number] % 2 != 0)
+            {
+                continue;
+            }
+
+            Console.WriteLine(number);
+        }
+    }
+}
