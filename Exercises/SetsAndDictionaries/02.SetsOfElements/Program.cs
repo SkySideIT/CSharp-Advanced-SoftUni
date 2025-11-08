98489706c60b39a80c55d33622eed10017e7c675
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-11-08 21:54:28 +0200

    Add files via upload

---
+++ Exercises/SetsAndDictionaries/02.SetsOfElements/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _02.SetsOfElements;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] parameters = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = parameters[0];
+        int m = parameters[1];
+
+        HashSet<int> first = new HashSet<int>();
+        HashSet<int> second = new HashSet<int>();
+        HashSet<int> equalElements = new HashSet<int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            first.Add(int.Parse(Console.ReadLine()));
+        }
+
+        for (int i = 0; i < m; i++)
+        {
+            second.Add(int.Parse(Console.ReadLine()));
+        }
+
+        foreach (int el in first)
+        {
+            if (second.Contains(el))
+            {
+                equalElements.Add(el);
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", equalElements));
+    }
+}
