e976ec728152f82059b64ad1fa6fe8d2afa08377
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-11-18 21:16:16 +0200

    Add files via upload

---
+++ Exercises/FunctionalProgramming/08.ListOfPredicates/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _08.ListOfPredicates;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int end = int.Parse(Console.ReadLine());
+        int[] dividers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+        Func<int, bool>[] condition = new Func<int, bool>[dividers.Length];
+        for (int i = 0; i < dividers.Length; i++)
+        {
+            int pos = i;
+            condition[pos] = x => x % dividers[pos] == 0;
+        }
+
+        List<int> result = Filter(1, end, condition);
+        Console.WriteLine(string.Join(" ", result));
+    }
+
+    static List<int> Filter(int start, int end, Func<int, bool>[] conditions)
+    {
+        List<int> result = new List<int>();
+
+        for (int i = start; i <= end; i++)
+        {
+            bool isMatch = true;
+            for (int j = 0; isMatch && j < conditions.Length; j++)
+            {
+                isMatch = conditions[j](i);
+            }
+
+            if (isMatch)
+            {
+                result.Add(i);
+            }
+        }
+
+        return result;
+    }
+}
