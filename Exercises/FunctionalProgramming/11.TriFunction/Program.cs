be421ee8586111b13287e0516e0e5c6c58a46e1f
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-11-18 21:18:07 +0200

    Add files via upload

---
+++ Exercises/FunctionalProgramming/11.TriFunction/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _11.TriFunction;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int num = int.Parse(Console.ReadLine());
+        string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        string result = First(names, x => NameIsSpecial(x, num));
+        Console.WriteLine(result);
+    }
+
+    static string First(string[] array, Func<string, bool> condition)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (condition(array[i]))
+            {
+                return array[i];
+            }
+        }
+
+        throw new InvalidOperationException("No elements match the given condition!");
+    }
+
+    static bool NameIsSpecial(string name, int n)
+    {
+        int sum = 0;
+        for (int i = 0; i < name.Length; i++)
+        {
+            sum += name[i];
+        }
+        return sum >= n;
+    }
+}
