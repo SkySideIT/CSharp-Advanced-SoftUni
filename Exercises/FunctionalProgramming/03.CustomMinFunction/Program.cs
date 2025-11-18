628bd5ece625f5fbe9290890332612e0c4e4f564
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-11-18 21:14:00 +0200

    Add files via upload

---
+++ Exercises/FunctionalProgramming/03.CustomMinFunction/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _03.CustomMinFunction;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        int min = Aggregate(numbers, (num, agg) => num < agg ? num : agg, int.MaxValue);
+        Console.WriteLine(min);
+    }
+
+    static int Aggregate(int[] array, Func<int, int, int> func, int initial)
+    {
+        int aggregate = initial;
+        foreach (int num in array)
+        {
+            aggregate = func(num, aggregate);
+        }
+
+        return aggregate;
+    }
+}
