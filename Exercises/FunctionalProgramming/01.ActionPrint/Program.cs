acfc5419761d4331a5df0cffb119c8a767ce5ab2
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-11-18 21:13:11 +0200

    Add files via upload

---
+++ Exercises/FunctionalProgramming/01.ActionPrint/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _01.ActionPrint;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        ForEach(names, w => Console.WriteLine(w));
+    }
+
+    static void ForEach(string[] array, Action<string> action)
+    {
+        foreach (string el in array)
+        {
+            action(el);
+        }
+    }
+}
