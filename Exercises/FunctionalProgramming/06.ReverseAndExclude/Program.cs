172e2b683f507b643e048b41dbc808414b5159c0
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-11-18 21:15:32 +0200

    Add files via upload

---
+++ Exercises/FunctionalProgramming/06.ReverseAndExclude/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _06.ReverseAndExclude;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        int devider = int.Parse(Console.ReadLine());
+
+        List<int> result = FilterInReverse(numbers, n => n % devider != 0);
+        Console.WriteLine(string.Join(" ", result));
+    }
+
+    static List<int> FilterInReverse(int[] array, Predicate<int> condition)
+    {
+        List<int> result = new List<int>();
+
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            if (condition(array[i]))
+            {
+                result.Add(array[i]);
+            }
+        }
+
+        return result;
+    }
+}
