adbfc936e60b19e49928a5fcf190b58a4ad89245
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-11-18 21:15:51 +0200

    Add files via upload

---
+++ Exercises/FunctionalProgramming/07.PredicateForNames/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _07.PredicateForNames;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int nameLength = int.Parse(Console.ReadLine());
+        string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        Filter(names, x => x.Length <= nameLength, x => Console.WriteLine(x));
+    }
+
+    static void Filter(string[] array, Predicate<string> condition, Action<string> action)
+    {
+        foreach (var el in array)
+        {
+            if (condition(el))
+            {
+                action(el);
+            }
+        }
+    }
+}
