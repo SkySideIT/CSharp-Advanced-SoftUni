3579eeebd9cf9b6a8408e4f4ae907d4593bc3d8e
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-11-18 21:13:43 +0200

    Add files via upload

---
+++ Exercises/FunctionalProgramming/02.KnightsOfHonor/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _02.KnightsOfHonor;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        ForEachPlus(names, w => Console.WriteLine($"Sir {w}"));
+    }
+
+    static void ForEachPlus(string[] array, Action<string> action)
+    {
+        foreach (string el in array)
+        {
+            action(el);
+        }
+    }
+}
