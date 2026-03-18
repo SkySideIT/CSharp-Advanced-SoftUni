537c19f116591e57d0a13a389415caf89b4bc35e
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2026-03-18 10:10:14 +0200

    Add files via upload

---
+++ Exercises/DefiningClasses/06.SpeedRacing/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.SpeedRacing;
+
+public class Car
+{
+    public string Model { get; set; }
+    public double FuelAmount { get; set; }
+    public double FuelConsumptionPerKilometer { get; set; }
+    public double TravelledDistance { get; set; }
+
+    public bool Drive(double distanceInKm)
+    {
+        double fuelNeeded = distanceInKm * this.FuelConsumptionPerKilometer;
+        if (fuelNeeded > this.FuelAmount)
+        {
+            return false;
+        }
+
+        this.FuelAmount -= fuelNeeded;
+        this.TravelledDistance += distanceInKm;
+        return true;
+    }
+}
---
+++ Exercises/DefiningClasses/06.SpeedRacing/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _06.SpeedRacing;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, Car> carsByModel = new Dictionary<string, Car>();
+
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] data = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Car car = new Car 
+            { 
+                Model = data[0],
+                FuelAmount = double.Parse(data[1]),
+                FuelConsumptionPerKilometer = double.Parse(data[2]),
+                TravelledDistance = 0
+            };
+
+            carsByModel[car.Model] = car;
+        }
+
+        string input;
+        while ((input = Console.ReadLine()) != "End")
+        {
+            string[] data = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string model = data[1];
+            double distanceInKm = double.Parse(data[2]);
+
+            Car car = carsByModel[model];
+
+            if (!car.Drive(distanceInKm))
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+
+        foreach (Car currentCar in carsByModel.Values)
+        {
+            Console.WriteLine($"{currentCar.Model} {currentCar.FuelAmount:f2} {currentCar.TravelledDistance}");
+        }
+    }
+}
