55fd9e3033316eff903b071bcf6d9617b54bf6f9
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2026-03-18 10:07:38 +0200

    Add files via upload

---
+++ Exercises/DefiningClasses/03.OldestFamilyMember/Family.cs
@@ -0,0 +1,16 @@
+﻿
+namespace _03.OldestFamilyMember;
+
+internal class Family
+{
+    private readonly List<Person> members = new List<Person>();
+    public void AddMember(Person person)
+    {
+        members.Add(person);
+    }
+
+    public Person GetOldestMember()
+    {
+        return this.members.MaxBy(x => x.Age);
+    }
+}
