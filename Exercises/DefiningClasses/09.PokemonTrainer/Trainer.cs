54e596c20fedb24e710965d9d57c2932a3e2b04f
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2026-03-18 10:13:23 +0200

    Add files via upload

---
+++ Exercises/DefiningClasses/09.PokemonTrainer/Trainer.cs
@@ -0,0 +1,13 @@
+﻿
+namespace _09.PokemonTrainer;
+public class Trainer
+{
+    public Trainer(string name)
+    {
+        this.Name = name;
+    }
+
+    public string Name { get; }
+    public int Badges { get; set; }
+    public List<Pokemon> Pokemons { get; } = new List<Pokemon>();
+}
