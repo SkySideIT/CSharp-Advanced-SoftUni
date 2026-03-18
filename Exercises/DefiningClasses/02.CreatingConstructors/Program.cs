42e618d9a363e1eefd6a921101085affe46850a3
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2026-03-18 10:06:39 +0200

    Add files via upload

---
+++ Exercises/DefiningClasses/02.CreatingConstructors/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _02.CreatingConstructor;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        Person person1 = new Person();
+        person1.Name = "Test";
+        person1.Age = 30;
+
+        Person person2 = new Person { Name = "Jose", Age = 24 };
+    }
+}
