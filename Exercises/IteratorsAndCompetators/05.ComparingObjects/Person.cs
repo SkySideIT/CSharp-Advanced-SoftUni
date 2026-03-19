2e2a4f9d87f2aebd2813505357bd4219cf3c4f06
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2026-03-19 03:01:02 +0200

    Add files via upload

---
+++ Exercises/IteratorsAndCompetators/05.ComparingObjects/Person.cs
@@ -0,0 +1,30 @@
+﻿
+namespace _05.ComparingObjects;
+
+public class Person : IComparable<Person>
+{
+    private readonly string _name;
+    private readonly int _age;
+    private readonly string _town;
+
+    public Person(string name, int age, string town)
+    {
+        this._name = name;
+        this._age = age;
+        this._town = town;
+    }
+
+    public int CompareTo(Person? other)
+    {
+        if (other == null) return 1;
+
+        int nameComparison = Comparer<string>.Default.Compare(this._name, other._name);
+        if (nameComparison != 0 ) return nameComparison;
+
+        int ageComparison = Comparer<int>.Default.Compare(this._age, other._age);
+        if (ageComparison != 0 ) return ageComparison;
+
+        int townComparison = Comparer<string>.Default.Compare(this._town, other._town);
+        return townComparison;
+    }
+}
---
+++ Exercises/IteratorsAndCompetators/05.ComparingObjects/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _05.ComparingObjects;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Person> people = new List<Person>();
+
+        string input;
+        while ((input = Console.ReadLine()) != "END")
+        {
+            string[] data = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Person person = new Person(data[0], int.Parse(data[1]), data[2]);
+            people.Add(person);
+        }
+
+        int position = int.Parse(Console.ReadLine());
+        Person requestedPerson = people[position - 1];
+
+        int matches = 0;
+        foreach (Person person in people)
+        {
+            if (Comparer<Person>.Default.Compare(requestedPerson, person) == 0)
+            {
+                matches++;
+            }
+        }
+
+        if (matches == 1)
+        {
+            Console.WriteLine("No matches");
+        }
+        else
+        {
+            Console.WriteLine($"{matches} {people.Count - matches} {people.Count}");
+        }
+    }
+}
