b7a836695bd5617649c67d244d5e09bc36de25f3
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2026-03-19 03:01:47 +0200

    Add files via upload

---
+++ Exercises/IteratorsAndCompetators/07.CustomComparator/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _07.CustomComparator;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+        IComparer<int> comparer = Comparer<int>.Create((x, y) =>
+        {
+            if (x % 2 == 0 && y % 2 != 0) return -1;
+            if (x % 2 != 0 && y % 2 == 0) return 1;
+
+            return Comparer<int>.Default.Compare(x, y);
+        });
+
+        Array.Sort(numbers, comparer);
+
+        Console.WriteLine(string.Join(' ', numbers));
+    }
+}
