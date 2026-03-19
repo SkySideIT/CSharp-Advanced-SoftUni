2784f4bcfe22d4d5ba5a2368dd1f79a9b9618851
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2026-03-19 02:58:26 +0200

    Add files via upload

---
+++ Exercises/IteratorsAndCompetators/01.ListyIterator/ListyIterator.cs
@@ -0,0 +1,49 @@
+﻿
+using System.Collections;
+
+namespace _01.ListyIterator;
+
+public class ListyIterator<T> : IEnumerable<T>
+{
+    private readonly List<T> _list;
+    private int _index;
+
+    public ListyIterator(IEnumerable<T> values)
+    {
+        this._list = new List<T>(values);
+    }
+
+    public T Current => this.GetCurrentElement();
+
+    public bool Move()
+    {
+        if (!HasNext()) return false;
+        
+        this._index++;
+        return true;
+    }
+
+    public bool HasNext()
+        => this._index + 1 < this._list.Count;
+
+    private T GetCurrentElement()
+    {
+        if (this._index < 0 || this._index > this._list.Count)
+        {
+            throw new InvalidOperationException("Invalid operation.");
+        }
+
+        return this._list[this._index];
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = 0; i < this._list.Count; i++)
+        {
+            yield return this._list[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+        => this.GetEnumerator();
+}
---
+++ Exercises/IteratorsAndCompetators/01.ListyIterator/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _01.ListyIterator;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string[] data = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        ListyIterator<string> listy = new ListyIterator<string>(data.Skip(1));
+
+        string command;
+        while ((command = Console.ReadLine()) != "END")
+        {
+            try
+            {
+                if (command == "HasNext") Console.WriteLine(listy.HasNext());
+                else if (command == "Move") Console.WriteLine(listy.Move());
+                else if (command == "Print") Console.WriteLine(listy.Current);
+                else if (command == "PrintAll") Console.WriteLine(string.Join(' ', listy));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
